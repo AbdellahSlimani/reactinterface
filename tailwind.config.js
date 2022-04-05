9f46b72426db47645f18fe7268e8198e9ece425d
Abdellah Slimani <85078022+Slimani-Abdellah@users.noreply.github.com>
2022-04-05 23:48:39 +0000

    third commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 module.exports = {
+  // purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [ require('@tailwindcss/forms')]
 }
